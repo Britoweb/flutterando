97975f005a75ad24f8c35b7072e6971b2a873202
Rafael Brito <rafaelbrito.web@gmail.com>
2021-07-13 19:57:52 -0300

    Fim Aula 17

---
+++ lib/login_page.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String email = '';
+  String password = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.width,
+          width: MediaQuery.of(context).size.height,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                  onChanged: (text) {
+                    email = text;
+                  },
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                      labelText: 'Email', border: OutlineInputBorder()),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextField(
+                  onChanged: (text) {
+                    password = text;
+                  },
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      labelText: 'Password', border: OutlineInputBorder()),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (email == 'a@b.com' &&
+                        password == '123') {
+                      print('Autenticando...');
+                    } else {
+                      print('Login Inválido');
+                    }
+                  },
+                  child: Text('Entrar'),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
