2c10d1d9672272b93763776e995b8d63086df10e
Rafael Brito <rafaelbrito.web@gmail.com>
2021-07-14 15:00:27 -0300

    Fim aula 19

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -49,9 +49,7 @@ class _LoginPageState extends State<LoginPage> {
                 ElevatedButton(
                   onPressed: () {
                     if (email == 'a@b.com' && password == '123') {
-                      Navigator.of(context).pushReplacement(
-                        MaterialPageRoute(builder: (context) => HomePage())
-                      );
+                      Navigator.of(context).pushReplacementNamed('/home');
                     } else {
                       print('Login Inválido');
                     }
