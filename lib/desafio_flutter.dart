dc744e49e119272a97193ecac2d6bce3440631ba
Rafael Brito <rafaelbrito.web@gmail.com>
2021-07-16 15:35:20 -0300

    Fim aula 27 - Desafio

--- lib/desafio_flutter.dart
+++ lib/desafio_flutter.dart
@@ -12,14 +12,43 @@ class _desafioState extends State<desafio> {
       body: Container(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
-        color: Colors.red.withOpacity(0.8),
-        child: Stack(
+        color: Colors.red.withOpacity(0.6),
+        child: Column(
+          
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-
+            Container(
+              
+                width: 100,
+                height: 100,
+                child: Image.asset('assets/images/logo_tinder.png')),
+            Padding(
+              padding: EdgeInsets.only(top: 30, bottom: 9),
+              child: const Text('Location Changer', 
+                style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20)
+              )
+            ),
+            const Text('Plugin app for Tinder', style: TextStyle(color: Colors.white, fontSize: 12)),
+            Padding(
+              padding: EdgeInsets.only(top: 20),
+              child: ElevatedButton(
+                
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0)
+                  )),
+                  backgroundColor: MaterialStateProperty.all(Colors.white),
+                ),
+                  onPressed: () {
+                    print('Legal funcionou!');
+                  },
+                  child: const Text('Login with Facebook', style: TextStyle(color: Colors.red),
+                  )),
+            )
           ],
         ),
       ),
-      
     );
   }
 }
